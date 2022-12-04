74a0349c45bb2b1b347771e5a004607d95d30a9f
AndreiCip <dinu.andrei0101@gmail.com>
2022-12-05 01:05:14 +0200

    updated

--- book_app/site/routes.py
+++ book_app/site/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import BooleanField, StringField, FileField, validators
+from models.models import User, Casino
 
 site = Blueprint('site', 
                 __name__, 
@@ -9,21 +12,30 @@ site = Blueprint('site',
                 )
 
 
-class RegistrationForm(Form):
+class SelfExclusionForm(FlaskForm):
     name = StringField('Name', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
+    cnp = StringField('Cnp', [validators.Length(min=13,max=13)])
+    number_phone = StringField('Number Phone', [validators.Length(min=13,max=13),
+                                                validators.DataRequired()
+                                                ])
+    email_address = StringField('Email Address', [validators.Length(min=6, max=60), 
+                                                validators.DataRequired()
+                                                ])
+    selfie_kyc = FileField('selfie', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png'], 'Images only!')
     ])
-    confirm = PasswordField('Repeat Password')
+    selected_casinos = StringField('Selected Casinos', [validators.DataRequired()])
     accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
 
 
 @site.route('/')
 
 def index():
-    return render_template('index.html')
+    form = SelfExclusionForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('index.html', form=form)
 
 
 @site.route('/submit-data', methods=['GET','POST'])
